c0097d1c43895da2699f768c79d33040788f1a19
junyh0730 <junyh0730@naver.com>
2019-08-12 17:24:03 +0900

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'EditPage.dart';
+import 'ListPage.dart';
 
 void main() => runApp(MyApp());
 
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: EditPage(),
+      home: TrashBinList(),
     );
   }
 }
